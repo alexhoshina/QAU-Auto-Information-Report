a9ae489a648ed35933b576585fd911e106bf4395
alexhoshina <92345812+alexhoshina@users.noreply.github.com>
2022-04-21 20:52:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import re
 import time
 
+#模式选择，数值为0或1，目前仅1模式可用
 model = 1
 Time = time.strftime("%Y-%m-%d", time.localtime())
 get_key_url = 'https://zhxg.qau.edu.cn/xuegong/api/BaseData/GetAuthCode'
@@ -29,12 +30,14 @@ User = {
     'TwoStitchDate':'xxxx-xx-xx'
 }    
 
+#获取key
 def get_key():
     key_origin = requests.post(get_key_url,  cookies=User['cookies'])
     print(key_origin.text)
     key_0 = re.search(r'Key...(.{36})', key_origin.text)
     return key_0.group(1)
 
+获取X-Token
 def get_token(key,username,userpassword):
     json_data = {
     'LoginName': '%s'%(username),
@@ -47,6 +50,7 @@ def get_token(key,username,userpassword):
     token_0 = re.search(r'Token":"(.+)","E', token_origin.text)
     return token_0.group(1)
 
+#上报数据获取模块，分为1模式（预设模式）和0模式（昨日模式），目前昨日模式存在无法解决的bug，请勿使用
 def get_data(token,Time):
     if(model):
         data = '{"NowDate":"%s","UserType":1,"Country":"%s","Province":"%s","City":"%s","County":"%s","street_number":"%s","CurrentPosition":"%s","FirstStitchDate":"%s","TwoStitchDate":"%s","ThreeStitchDate":null,"TodaySchedule":0,"BRHGTJZRSFYXLQKOne":0,"BRHGTJZRSFYXLQKTwo":0,"SZSQTNSFFSYQ":0,"LJSHJCS":0,"BRHGTJZRSFYYXZZWQY":0,"JRSFCSWFL":0,"JRSFCSNQTDJSFQ":0,"DayTemperature":1,"Vaccination":2,"Remark":""}'%(User['NowDate'],User['Country'],User['Province'],User['City'],User['County'],User['street_number'],User['CurrentPosition'],User['FirstStitchDate'],User['TwoStitchDate'])
@@ -58,11 +62,10 @@ def get_data(token,Time):
         data_0 = requests.get(get_data_url, headers=headers, cookies=User['cookies'])
         print(data_0.text)
         data = data_0.text
-        print('\n\n\n%s\n\n\n'%data)
         data = re.sub(r'"NowDate":null', '"NowDate":"%s"'%Time, data)
-    print('\n\n\n%s\n\n\n'%data)
     return data
 
+#上报模块
 def enter(data,token):
     headers = {
     'X-Token': token,
@@ -72,4 +75,5 @@ def enter(data,token):
     response.enconding="utf-8"
     print('\n\n%s' % response.text)
 
+#运行上报
 enter(get_data(get_token(get_key(),User['username'],User['userpassword']),User['NowDate']),get_token(get_key(),User['username'],User['userpassword']))
