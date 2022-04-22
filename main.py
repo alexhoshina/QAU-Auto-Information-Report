735df77208cc41edab2d229744e2e467b1da87e8
alexhoshina <2489545931@qq.com>
2022-04-22 09:55:09 +0800

    优化代码

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 import re
 import time
+import json
 
-model = 1
 Time = time.strftime("%Y-%m-%d", time.localtime())
 get_key_url = 'https://zhxg.qau.edu.cn/xuegong/api/BaseData/GetAuthCode'
 get_token_url = 'https://zhxg.qau.edu.cn/xuegong/api/UserAuth/GetManUserLogin'
@@ -12,26 +12,15 @@ enter_url = 'https://zhxg.qau.edu.cn/xuegong/api/DayVirus/AddVirus'
 requests.get(get_key_url,get_token_url, verify=False)
 requests.get(enter_url,get_data_url, verify=False)
 
-User = {
-    'username' : 'your name',
-    'userpassword' : 'your password',
-    'cookies' : {'your cookies'},
-    'NowDate' : '%s'%(Time),
-    'Country': '中国',
-    'Province':'山东省',
-    'City':'青岛市',
-    'County':'平度市',
-    'street_number':'海信路',
-    'CurrentPosition':'中国山东省青岛市平度市海信路',
-    # 您第一针疫苗接种时间
-    'FirstStitchDate':'xxxx-xx-xx',
-    # 您第二针疫苗接种时间
-    'TwoStitchDate':'xxxx-xx-xx'
-}    
+with open('config.json','r', encoding='UTF-8') as f:
+    User = json.load(f)
+    
+with open('config.json','r', encoding='UTF-8') as f:
+    data = json.load(f)
+    data['NowDate'] = '%s'%(Time)
 
 def get_key():
     key_origin = requests.post(get_key_url,  cookies=User['cookies'])
-    print(key_origin.text)
     key_0 = re.search(r'Key...(.{36})', key_origin.text)
     return key_0.group(1)
 
@@ -43,13 +32,18 @@ def get_token(key,username,userpassword):
     'ApplyType': 1,
     }
     token_origin = requests.post(get_token_url,  cookies=User['cookies'], json=json_data)
-    print(token_origin.text)
     token_0 = re.search(r'Token":"(.+)","E', token_origin.text)
     return token_0.group(1)
 
-def get_data(token,Time):
-    if(model):
-        data = '{"NowDate":"%s","UserType":1,"Country":"%s","Province":"%s","City":"%s","County":"%s","street_number":"%s","CurrentPosition":"%s","FirstStitchDate":"%s","TwoStitchDate":"%s","ThreeStitchDate":null,"TodaySchedule":0,"BRHGTJZRSFYXLQKOne":0,"BRHGTJZRSFYXLQKTwo":0,"SZSQTNSFFSYQ":0,"LJSHJCS":0,"BRHGTJZRSFYYXZZWQY":0,"JRSFCSWFL":0,"JRSFCSNQTDJSFQ":0,"DayTemperature":1,"Vaccination":2,"Remark":""}'%(User['NowDate'],User['Country'],User['Province'],User['City'],User['County'],User['street_number'],User['CurrentPosition'],User['FirstStitchDate'],User['TwoStitchDate'])
+def get_data(token,Time,data):
+    if(User['model']):
+        del data['username']
+        del data['cookies']
+        del data['userpassword']
+        del data['model']
+        print(data)
+        data_str = json.dumps(data,ensure_ascii=False)
+        print(data_str)
     else:
         headers = {
             'Accept': 'application/json, text/plain, */*',
@@ -58,18 +52,18 @@ def get_data(token,Time):
         data_0 = requests.get(get_data_url, headers=headers, cookies=User['cookies'])
         print(data_0.text)
         data = data_0.text
-        print('\n\n\n%s\n\n\n'%data)
         data = re.sub(r'"NowDate":null', '"NowDate":"%s"'%Time, data)
-    print('\n\n\n%s\n\n\n'%data)
-    return data
+        data = re.sub(r'"County":""', '"County":"%s","CurrentPosition":"%s"'%(User['County'],User['CurrentPosition']), data)
+        data_str = re.search(r'({"Id".+)',data).group(1)
+    return data_str
 
 def enter(data,token):
     headers = {
     'X-Token': token,
     'Content-Type': 'application/json;charset=UTF-8',
     }
-    response = requests.post(enter_url, headers=headers, cookies=User['cookies'], data=data.encode("UTF-8"))#
+    response = requests.post(enter_url, headers=headers, cookies=User['cookies'], data=data.encode("utf-8"))
     response.enconding="utf-8"
     print('\n\n%s' % response.text)
 
-enter(get_data(get_token(get_key(),User['username'],User['userpassword']),User['NowDate']),get_token(get_key(),User['username'],User['userpassword']))
+enter(get_data(get_token(get_key(),User['username'],User['userpassword']),User['NowDate'],data),get_token(get_key(),User['username'],User['userpassword']))
